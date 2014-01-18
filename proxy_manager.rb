516e958cb63b40ab50d81ded6c28c3ab40c1ad15
Kirill Platonov <platonov.kd@gmail.com>
2014-01-18 16:43:32 +0400

    add getting of first proxy

--- proxy_manager.rb
+++ proxy_manager.rb
@@ -1,6 +1,9 @@
 class ProxyManager
+  attr_reader :proxy, :proxies
+
   def initialize(input = "proxies.txt")
     import_from input
+    get_proxy
   end
 
   private
@@ -13,5 +16,12 @@ class ProxyManager
         proxy = line.split(":")
         @proxies << [proxy[0],proxy[1]]
       end
+    end
+
+    def get_proxy
+      @proxy = @proxies[0]
+      # move first element to the end
+      @proxies.delete_at(0)
+      @proxies << @proxy
     end
 end
