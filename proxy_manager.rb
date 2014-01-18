1d37dac448ed350331e2145ae315c4e64ff01a3e
Kirill Platonov <platonov.kd@gmail.com>
2014-01-18 16:23:26 +0400

    add import

--- proxy_manager.rb
+++ proxy_manager.rb
@@ -1,5 +1,17 @@
 class ProxyManager
   def initialize(input = "proxies.txt")
-    @proxies = input.is_a?(Array) ? input : import_from_source(input)
+    import_from input
   end
+
+  private
+
+    def import_from(input)
+      @proxies = Array.new
+      source = input.is_a?(Array) ? input : File.open(input, "r")
+
+      source.each do |line|
+        proxy = line.split(":")
+        @proxies << [proxy[0],proxy[1]]
+      end
+    end
 end
