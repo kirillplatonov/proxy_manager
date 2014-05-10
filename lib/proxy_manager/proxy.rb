476bd6340af636702d42173c03a1e681331f9c54
Kirill Platonov <platonov.kd@gmail.com>
2014-05-10 07:44:40 +0400

    refactor get method

--- lib/proxy_manager/proxy.rb
+++ lib/proxy_manager/proxy.rb
@@ -14,62 +14,11 @@ module ProxyManager
     end
 
     def get(count = 1)
-      raise 'List is empty' if @list.empty?
-
-      items = []
-      new_list = @list.clone
-
-      @list.each_with_index do |proxy, key|
-        new_list.shift
-        new_list << proxy
-
-        if count == 1
-          items = proxy
-          break
-        else
-          items << proxy
-          break if items.size == count
-        end
-      end
-
-      @list = new_list
-
-      raise 'There are no available proxy' if items.empty?
-
-      save_to_file(@list_file, @list) if @list_file
-
-      items
+      get_proxy(count)
     end
 
     def get!(count = 1)
-      raise 'List is empty' if @list.empty?
-
-      items = []
-      new_list = @list.clone
-
-      @list.each_with_index do |proxy, key|
-        new_list.shift
-
-        if self.class.connectable? proxy
-          new_list << proxy
-
-          if count == 1
-            items = proxy
-            break
-          else
-            items << proxy
-            break if items.size == count
-          end
-        end
-      end
-
-      @list = new_list
-
-      raise 'There are no available proxy' if items.empty?
-
-      save_to_file(@list_file, @list) if @list_file
-
-      items
+      get_proxy(count, true)
     end
 
     def self.connectable?(proxy)
@@ -101,6 +50,37 @@ module ProxyManager
         result << [ip, port.to_i] if ip.is_a?(String) && port.is_a?(String)
       end
       result
+    end
+
+    def get_proxy(count, check_connection = false)
+      raise 'List is empty' if @list.empty?
+
+      items = []
+      new_list = @list.clone
+
+      @list.each_with_index do |proxy, key|
+        new_list.shift
+
+        if !check_connection || self.class.connectable?(proxy)
+          new_list << proxy
+
+          if count == 1
+            items = proxy
+            break
+          else
+            items << proxy
+            break if items.size == count
+          end
+        end
+      end
+
+      @list = new_list
+
+      raise 'There are no available proxy' if items.empty?
+
+      save_to_file(@list_file, @list) if @list_file
+
+      items
     end
 
     def save_to_file(file, list)
